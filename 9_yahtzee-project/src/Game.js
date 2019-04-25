f3073394351ac3c6f94adc4509ed35a888b566a0
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-25 14:33:58 -0400

    fix extra rolls bug

--- 9_yahtzee-project/src/Game.js
+++ 9_yahtzee-project/src/Game.js
@@ -44,14 +44,11 @@ class Game extends Component {
 
   toggleLocked = idx => {
     // toggle whether idx is in locked or not
-    this.setState(st => ({
-      locked: st.locked.map((val, i) => (idx === i ? !val : val)),
-      // locked: [
-      //   ...st.locked.slice(0, idx),
-      //   !st.locked[idx],
-      //   ...st.locked.slice(idx + 1),
-      // ],
-    }))
+    if (this.state.rollsLeft > 0) {
+      this.setState(st => ({
+        locked: st.locked.map((val, i) => (idx === i ? !val : val)),
+      }))
+    }
   }
 
   doScore = (rulename, ruleFn) => {
@@ -80,7 +77,9 @@ class Game extends Component {
             <div className="Game-button-wrapper">
               <button
                 className="Game-reroll"
-                disabled={this.state.locked.every(x => x)}
+                disabled={
+                  this.state.locked.every(x => x) || this.state.rollsLeft === 0
+                }
                 onClick={this.roll}
               >
                 {this.state.rollsLeft} Rerolls Left
