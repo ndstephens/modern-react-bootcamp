f36a52775c118452e6ed3219d3a56ed8573bd4b5
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-26 15:57:06 -0400

    small refactors

--- 10_deck-of-cards-project/src/components/CardGame.js
+++ 10_deck-of-cards-project/src/components/CardGame.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 import './CardGame.css'
-
 import Card from './Card'
 
 class CardGame extends Component {
@@ -19,19 +18,19 @@ class CardGame extends Component {
     this.setState({ deckId: deck.data.deck_id })
   }
 
-  handleClick = async () => {
+  getCard = async () => {
     const response = await axios.get(
       `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/`
     )
 
     const rotate = Math.floor(Math.random() * 81 - 40)
-    const transX = Math.floor(Math.random() * 21 - 10)
-    const transY = Math.floor(Math.random() * 21 - 10)
-    const style = {
+    const transX = Math.floor(Math.random() * 41 - 20)
+    const transY = Math.floor(Math.random() * 41 - 20)
+    const transformStyle = {
       transform: `translateX(calc(-50% + ${transX}px)) translateY(${transY}px) rotate(${rotate}deg)`,
     }
 
-    const newCard = { ...response.data.cards[0], style: style }
+    const newCard = { ...response.data.cards[0], style: transformStyle }
 
     this.setState(prevSt => ({
       cardsRemaining: response.data.remaining,
@@ -44,10 +43,12 @@ class CardGame extends Component {
 
     return (
       <div className="CardGame">
-        <button onClick={this.handleClick} disabled={cardsRemaining === 0}>
+        <button onClick={this.getCard} disabled={cardsRemaining === 0}>
           Gimme a Card
         </button>
+
         <p>Cards Remaining: {cardsRemaining}</p>
+
         <div className="CardGame__cards">
           {cards.map(card => (
             <Card {...card} key={card.code} />
