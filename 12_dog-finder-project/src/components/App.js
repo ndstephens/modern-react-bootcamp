06cd4d3227f3c68b787402a27278a5da2fe24ae3
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-28 12:13:57 -0400

    dog links

--- 12_dog-finder-project/src/components/App.js
+++ 12_dog-finder-project/src/components/App.js
@@ -12,7 +12,9 @@ class App extends Component {
     return (
       <div>
         <Navbar dogs={this.props.dogs} />
-        <Routes dogs={this.props.dogs} />
+        <div className="container">
+          <Routes dogs={this.props.dogs} />
+        </div>
       </div>
     )
   }
--- 12_dog-finder-project/src/components/DogList.js
+++ 12_dog-finder-project/src/components/DogList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './DogList.css'
 
 class DogList extends Component {
@@ -6,15 +7,15 @@ class DogList extends Component {
     return (
       <div className="DogList">
         <h1 className="display-1 text-center">Dog List!</h1>
-        <div className="container">
-          <div className="row">
-            {this.props.dogs.map(dog => (
-              <div className="DogList__dog col-md-4 text-center" key={dog.name}>
-                <img src={dog.src} alt={dog.name} />
-                <h3>{dog.name}</h3>
-              </div>
-            ))}
-          </div>
+        <div className="row">
+          {this.props.dogs.map(dog => (
+            <div className="DogList__dog col-md-4 text-center" key={dog.name}>
+              <img src={dog.src} alt={dog.name} />
+              <h3>
+                <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
+              </h3>
+            </div>
+          ))}
         </div>
       </div>
     )
