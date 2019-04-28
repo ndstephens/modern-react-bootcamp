56abcb59774bf1ad9ccf835903f65fefe0649a74
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-28 12:39:03 -0400

    styling

--- 12_dog-finder-project/src/components/DogList.js
+++ 12_dog-finder-project/src/components/DogList.js
@@ -6,13 +6,15 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1 text-center">Dog List!</h1>
+        <h1 className="display-1 text-center mt-3 mb-5">Dog List!</h1>
         <div className="row">
           {this.props.dogs.map(dog => (
             <div className="DogList__dog col-md-4 text-center" key={dog.name}>
               <img src={dog.src} alt={dog.name} />
               <h3>
-                <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
+                <Link className="underline" to={`/dogs/${dog.name}`}>
+                  {dog.name}
+                </Link>
               </h3>
             </div>
           ))}
