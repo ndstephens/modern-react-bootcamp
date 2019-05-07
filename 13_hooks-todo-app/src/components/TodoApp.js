c69f18a08403d7353e904055db77e5b9acb32a23
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-07 12:30:10 -0400

    project init, scaffold basic UI

---
+++ 13_hooks-todo-app/src/components/TodoApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+import TodoList from './TodoList'
+
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Grid from '@material-ui/core/Grid'
+
+function TodoApp(props) {
+  const initialTodos = [
+    { id: 1, task: 'Clean fishtank', completed: false },
+    { id: 2, task: 'Wash car', completed: true },
+    { id: 3, task: 'Grow beard', completed: false },
+  ]
+
+  const [todos, setTodos] = useState(initialTodos)
+
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa',
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: '64px' }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  )
+}
+
+export default TodoApp
---
+++ 13_hooks-todo-app/src/components/TodoList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Paper from '@material-ui/core/Paper'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+
+function TodoList({ todos }) {
+  return (
+    <Paper>
+      <List>
+        {todos.map(todo => (
+          <>
+            <ListItem>
+              <ListItemText>{todo.task}</ListItemText>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
+    </Paper>
+  )
+}
+
+export default TodoList
