49571624737ea64a97512abc1feef5677905d495
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-14 13:43:54 -0400

    hookify LanguageContext

--- 15_context-hooks/src/contexts/LanguageContext.js
+++ 15_context-hooks/src/contexts/LanguageContext.js
@@ -1,19 +1,15 @@
-import React, { Component, createContext } from 'react'
+import React, { useState, createContext } from 'react'
 
 export const LanguageContext = createContext()
 
-export default class LanguageProvider extends Component {
-  state = { language: 'FR' }
+export default function LanguageProvider(props) {
+  const [language, setLanguage] = useState('FR')
 
-  updateLanguage = e => this.setState({ language: e.target.value })
+  const updateLanguage = e => setLanguage(e.target.value)
 
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, updateLanguage: this.updateLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    )
-  }
+  return (
+    <LanguageContext.Provider value={{ language, updateLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  )
 }
